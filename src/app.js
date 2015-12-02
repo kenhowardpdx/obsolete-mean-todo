26aa4f47caa804ea6c387ed9bca199786a6d7faa
Ken Howard <ken@simplicatedweb.com>
2015-12-01 22:16:11 -0800

    Added GET route for todos

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ var app = express();
 
 app.use('/', express.static('public'));
 
+var apiRouter = express.Router();
+
+app.use('/api', apiRouter);
+
+apiRouter.get('/todos', function (req, res) {
+	res.json({ todos: [] });
+});
+
+// TODO: Add POST Route to create new entries
+
+// TODO: Add PUT Route to update existing entries
+
+// TODO: Add DELETE Route to remove existing entries
+
 app.listen(3000, function() {
 	console.log('The frontend server is running on port 3000!');
 });
