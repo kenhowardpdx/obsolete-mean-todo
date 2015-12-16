70137b62a0c0c6fc15b14e309f5414f0a6013fa3
Ken Howard <ken@simplicatedweb.com>
2015-12-15 19:18:09 -0800

    Added database connection.

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ var express = require('express');
 
 var app = express();
 
+require('./database');
+
 app.use('/', express.static('public'));
 
 app.use('/api', require(__dirname + '/api'));
---
+++ src/database.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/mean-todo', function(err) {
+	if (err) {
+		return console.log("Failed connecting to mongo!");
+	} else {
+		return console.log("Successfully connected to mongo!");
+	}
+});
